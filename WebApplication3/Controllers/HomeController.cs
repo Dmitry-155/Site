100d8ff0c2c56452dd82c92ffea3e30e70e847dc
Дмитрий Загребин <essergame@gmail.com>
2020-05-23 02:11:46 +0300

    Изменение 4

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -1,9 +1,4 @@
 using modelsTraining2.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 
 namespace modelsTraining2.Controllers
 {
